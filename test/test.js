fa2aec653f17b67b0d2d2cf3ff93ec23b292d89e
spandit812 <spandit@ishir.com>
2017-08-04 12:45:09 +0530

    changed package.json

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var expect=require('chai').expect;
 describe('lets describe',function(){
     it('execute it',function(){
-        expect(42).to.equal(41);
+        expect(42).to.equal(42);
     })
 })
